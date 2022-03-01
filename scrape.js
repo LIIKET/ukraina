08bdb8b67de19d4b379fbdc3c425ead34ef3b407
Anton Eriksson <anton.eriksson@vocean.com>
2022-03-01 22:01:44 +0100

    initial

---
+++ scrape.js
@@ -0,0 +1,47 @@
+const https = require('https');
+const fs = require('fs');
+
+async function scrapeValues() {
+    return new Promise((resolve, reject) => {
+        
+        https.get(`https://unicef.se/egna-insamlingar/7435-stand-with-ukraine-ett-insamlingslajv-i-solidaritet-med-folket-i-ukraina`, res => { 
+
+            let data = [];    
+            res.on('data', chunk => {
+                data.push(chunk);
+            });
+          
+            res.on('end', () => {
+                const html = Buffer.concat(data).toString();
+                const value = html.split('AmountCard-progressAmount">').pop().split('kr')[0].trim();
+                const percent = html.split('AmountCard-text">').pop().split('%')[0].trim();
+                resolve({ value: value, percent: percent });
+            });
+
+        }).on('error', err => {
+            reject(err);
+        });
+    });
+}
+
+(async () => {
+
+    while(true){
+     
+        try{
+            console.log("SCRAPING");
+            let result = await scrapeValues();
+            fs.writeFileSync('donations.json', JSON.stringify(result));
+        }
+        catch(err){
+            console.log(`SCRAPING ERROR ${err}`);
+        }
+        
+        console.log('WAITING');
+        await new Promise(resolve => setTimeout(resolve, 5000));
+    }
+
+})();
+
+
+
